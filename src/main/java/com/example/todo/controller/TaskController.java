ad2a7dd65ba3591107d4e6d887438c41740a60cd
ita-jp <tn00gm@gmail.com>
2022-09-13 22:29:51 +0900

    復数の TaskDTO を View で表示しよう

--- src/main/java/com/example/todo/controller/TaskController.java
+++ src/main/java/com/example/todo/controller/TaskController.java
@@ -5,13 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @RequestMapping("/")
 @Controller
 public class TaskController {
 
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("task", new TaskDTO(1, "機能Aを実装する", false));
+        var taskList = List.of(
+                new TaskDTO(1, "機能Aを実装する", true),
+                new TaskDTO(2, "機能Bを実装する", false),
+                new TaskDTO(3, "機能Cを実装する", false)
+        );
+        model.addAttribute("taskList", taskList);
         return "index";
     }
 }
