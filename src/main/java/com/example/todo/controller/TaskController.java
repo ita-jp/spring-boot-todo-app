c4d6f78bf4d0f58ad37b6a2da4b128ed40b340c1
ita-jp <tn00gm@gmail.com>
2022-09-13 22:04:37 +0900

    開発効率化のための IntelliJ の設定をしよう

--- src/main/java/com/example/todo/controller/TaskController.java
+++ src/main/java/com/example/todo/controller/TaskController.java
@@ -3,15 +3,13 @@ package com.example.todo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/")
 @Controller
 public class TaskController {
 
     @GetMapping
-    @ResponseBody
     public String index() {
-        return "<h1>Hello, world!!</h1>";
+        return "index";
     }
 }
