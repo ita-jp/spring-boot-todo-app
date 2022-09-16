00ff16961e4eb671c085a7eb9cd44104a99aa600
ita-jp <tn00gm@gmail.com>
2022-09-16 21:42:01 +0900

    test. TaskController

---
+++ src/test/java/com/example/todo/controller/TaskControllerTest.java
@@ -0,0 +1,46 @@
+package com.example.todo.controller;
+
+import com.example.todo.service.TaskEntity;
+import com.example.todo.service.TaskService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(TaskController.class)
+class TaskControllerTest {
+
+    @MockBean
+    private TaskService mockTaskService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnTask() throws Exception {
+        // ## Arrange ##
+        var taskEntity = new TaskEntity(987, "__title__", true);
+        when(mockTaskService.findAll()).thenReturn(List.of(taskEntity));
+
+        // ## Act ##
+        // ## Assert ##
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attribute("taskList", List.of(new TaskDTO(987, "__title__", true))))
+                .andExpect(content().string(containsString("987")))
+                .andExpect(content().string(containsString("__title__")))
+                .andExpect(content().string(containsString("完了")));
+    }
+
+}
