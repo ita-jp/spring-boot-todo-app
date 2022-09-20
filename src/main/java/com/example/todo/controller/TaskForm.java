04f6c4fd13f7382a5d78168495d527cca646a78a
ita-jp <tn00gm@gmail.com>
2022-09-20 17:32:29 +0900

    validation

--- src/main/java/com/example/todo/controller/TaskForm.java
+++ src/main/java/com/example/todo/controller/TaskForm.java
@@ -1,8 +1,11 @@
 package com.example.todo.controller;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public record TaskForm(
-        @NotBlank String title
+        @NotBlank
+        @Size(max = 256)
+        String title
 ) {
 }
